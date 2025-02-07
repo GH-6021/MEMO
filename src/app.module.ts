feb328566fa1ae580cc3e239cdd4afaaf47a061a
GH-6021 <rkgus0109@gmail.com>
2025-02-07 23:17:38 +0900

    포트수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { HttpModule } from '@nestjs/axios';
     TypeOrmModule.forRoot({
       type: 'mariadb',
       host: '127.0.0.1',
-      port: 3310,
+      port: 3306,
       username: 'test',
       password: 'test',
       database: 'memo',
